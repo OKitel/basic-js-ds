87d19dea6d51890b42ab5875961bb9d0f1be20bb
OKitel <kitelolga@gmail.com>
2023-04-17 16:58:35 +0200

    feat: add solution for binary search tree task

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,49 +1,134 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
-* Implement simple binary search tree according to task description
-* using Node from extensions
-*/
+ * Implement simple binary search tree according to task description
+ * using Node from extensions
+ */
 class BinarySearchTree {
+  constructor() {
+    this._root = null;
+  }
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this._root;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    const node = new Node(data);
+    if (this._root === null) {
+      this._root = node;
+    } else {
+      this._insertNode(this._root, node);
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  _insertNode(root, node) {
+    if (node.data < root.data) {
+      if (root.left === null) {
+        root.left = node;
+      } else {
+        this._insertNode(root.left, node);
+      }
+    } else {
+      if (root.right === null) {
+        root.right = node;
+      } else {
+        this._insertNode(root.right, node);
+      }
+    }
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    return hasValue(this._root, data);
+
+    function hasValue(root, value) {
+      if (!root) {
+        return false;
+      }
+
+      if (root.data === value) {
+        return true;
+      }
+
+      return value < root.data
+        ? hasValue(root.left, value)
+        : hasValue(root.right, value);
+    }
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data, root = this._root) {
+    if (root === null) {
+      return null;
+    } else if (data < root.data) {
+      return this.find(data, root.left);
+    } else if (data > root.data) {
+      return this.find(data, root.right);
+    } else {
+      return root;
+    }
+  }
+
+  remove(data) {
+    this._root = this._removeNode(this._root, data);
+  }
+
+  _removeNode(root, value) {
+    if (root === null) {
+      return null;
+    } else if (value < root.data) {
+      root.left = this._removeNode(root.left, value);
+      return root;
+    } else if (value > root.data) {
+      root.right = this._removeNode(root.right, value);
+      return root;
+    } else {
+      if (root.left === null && root.right === null) {
+        root = null;
+        return root;
+      }
+      if (root.left === null) {
+        root = root.right;
+        return root;
+      }
+      if (root.right === null) {
+        root = root.left;
+        return root;
+      }
+
+      let minRightValue = root.right;
+      while (minRightValue.left) {
+        minRightValue = minRightValue.left;
+      }
+
+      root.data = minRightValue.data;
+      root.right = this._removeNode(root.right, minRightValue.data);
+      return root;
+    }
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this._root) {
+      return null;
+    }
+    let node = this._root;
+    while (node.left) {
+      node = node.left;
+    }
+    return node.data;
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this._root) {
+      return null;
+    }
+    let node = this._root;
+    while (node.right) {
+      node = node.right;
+    }
+    return node.data;
   }
 }
 
 module.exports = {
-  BinarySearchTree
+  BinarySearchTree,
 };
